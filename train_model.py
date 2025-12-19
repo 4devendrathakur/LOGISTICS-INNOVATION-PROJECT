8a315561ae9ea65360663c04d4476614222d7e2a
Devendra Singh <116613958+4devendrathakur@users.noreply.github.com>
2025-12-19 09:17:22 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -2,16 +2,15 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import pickle
+import os
 
-
-# Load datasets
+# ---------------- LOAD DATA ----------------
 orders = pd.read_csv("data/orders.csv")
 delivery = pd.read_csv("data/delivery_performance.csv")
 routes = pd.read_csv("data/routes_distance.csv")
 costs = pd.read_csv("data/cost_breakdown.csv")
 
-# Clean column names
-
+# ---------------- CLEAN COLUMNS ----------------
 def clean_columns(df):
     df.columns = (
         df.columns
@@ -26,29 +25,28 @@ delivery = clean_columns(delivery)
 routes = clean_columns(routes)
 costs = clean_columns(costs)
 
-# Merge datasets on order_id
+# ---------------- MERGE DATA ----------------
 df = orders.merge(delivery, on="order_id", how="inner")
 df = df.merge(routes, on="order_id", how="inner")
 df = df.merge(costs, on="order_id", how="inner")
 
-# Handle missing values
+# ---------------- HANDLE MISSING VALUES ----------------
 df.fillna(df.median(numeric_only=True), inplace=True)
 df.fillna("none", inplace=True)
 
-# Target variable
-
+# ---------------- TARGET VARIABLE ----------------
 df["delayed"] = (
     df["actual_delivery_days"] > df["promised_delivery_days"]
 ).astype(int)
 
-# Encode Priority
+# ---------------- ENCODE PRIORITY ----------------
 df["priority"] = df["priority"].map({
     "economy": 0,
     "standard": 1,
     "express": 2
 })
 
-# Feature selection
+# ---------------- FEATURE SELECTION ----------------
 features = [
     "distance_km",
     "fuel_consumption_l",
@@ -63,22 +61,20 @@ features = [
 X = df[features]
 y = df["delayed"]
 
-# Train-test split
-
+# ---------------- TRAIN TEST SPLIT ----------------
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Train model
+# ---------------- MODEL TRAINING ----------------
 model = RandomForestClassifier(
     n_estimators=150,
     random_state=42
 )
 model.fit(X_train, y_train)
 
-# Save model
+# ---------------- SAVE MODEL ----------------
 MODEL_PATH = os.path.join("model", "delay_model.pkl")
-model = pickle.load(open(MODEL_PATH, "rb"))
+pickle.dump(model, open(MODEL_PATH, "wb"))
 
-
-print("Model trained successfully with real logistics data")
+print("✅ Logistics Delay Prediction Model Trained Successfully")
